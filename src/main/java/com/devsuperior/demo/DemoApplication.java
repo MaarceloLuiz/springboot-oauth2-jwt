13469f312eabf96f8dfdbc0a6aba1a2b98b65aed
MaarceloLuiz <marceloluizgpereira@gmail.com>
2025-01-26 16:35:22 +0000

    feat: implement GrantAuthority and UserDetails interface

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -7,17 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-
+public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println(passwordEncoder.encode("123456"));
-		System.out.println(passwordEncoder.matches("123456", "$2a$10$I3diDVBzCE4Va5WQZ5i7DeChtcAFSqPqiDrbD8.qiR77cLHFQ5Up2"));
 	}
 }
