f139ad4c6a96421e1ce0d53546b1fe6ac6ed418e
MaarceloLuiz <marceloluizgpereira@gmail.com>
2025-01-26 17:34:08 +0000

    feat: add user projection, repo and service, modify SecurityConfig to allow access to h2

---
+++ src/main/java/com/devsuperior/demo/services/UserService.java
@@ -0,0 +1,35 @@
+package com.devsuperior.demo.services;
+
+import com.devsuperior.demo.entities.Role;
+import com.devsuperior.demo.entities.User;
+import com.devsuperior.demo.projections.UserDetailsProjection;
+import com.devsuperior.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<UserDetailsProjection> users = userRepository.searchUserAndRolesByEmail(username);
+        if (users.isEmpty()) throw new UsernameNotFoundException("User not found");
+
+        User user = new User();
+        user.setEmail(username);
+        user.setPassword(users.get(0).getPassword());
+        users.forEach(projection ->
+                user.addRole(new Role(projection.getRoleId(), projection.getAuthority()))
+        );
+
+        return user;
+    }
+}
